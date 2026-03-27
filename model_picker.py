7eb8a95e93151d028a516f715b2168512a5915c8
snow <githubwithsnow@gmail.com>
2026-03-27 23:23:59 +0530

    feat(thinker): add initial function skeleton for model_picker

---
+++ model_picker.py
@@ -0,0 +1,14 @@
+# takes relevant info and returns suitable model(s)
+
+def model_picker(task: dict, n_rows: int, n_feat: int, ftype: dict, noise, style) :
+    '''
+    task = {type : reg|class, subtype : bin|multi|ord|None}
+    ftype = {}
+    noise =
+    style =
+    '''
+
+    return()
+
+if (__name__=="__main__") :
+    
